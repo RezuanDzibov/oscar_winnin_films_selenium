6e65635b56e121cad1573b7461dd33493968ebf2
RezuanDzibov <rezuan.dzbov@gmail.com>
2022-01-09 20:47:11 +0300

    Crawler can locate movies table and iterate by years.

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
+from typing import Type
+
 from selenium.webdriver import Chrome
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.remote.webdriver import WebDriver
+
+
+def webdriver_wait(driver: Type[WebDriver], timeout: int) -> WebDriverWait:
+    return WebDriverWait(driver=driver, timeout=timeout)
 
 
 class MainCrawler:
@@ -9,10 +17,13 @@ class MainCrawler:
         
     def scrap(self):
         self.driver.get('https://www.scrapethissite.com/pages/ajax-javascript/')
-        year_links = self.driver.find_elements(By.CLASS_NAME, 'year-link')
+        year_links: list = reversed(self.driver.find_elements(By.CLASS_NAME, 'year-link')) # type: ignore
         for year_link in year_links:
             year_link.click()
-
+            movie_table = webdriver_wait(driver=self.driver, timeout=3).until(       # type: ignore
+                EC.visibility_of_element_located((By.ID, 'table-body'))
+            )
+            
     # def __exit__(self, exc_type, exc_val, exc_tb):
     #     self.driver.close()
     
