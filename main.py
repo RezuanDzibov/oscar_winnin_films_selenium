d11a990c61571268b7063921120e586b1c744bd8
RezuanDzibov <rezuan.dzbov@gmail.com>
2022-01-09 21:46:52 +0300

    Wrote main the crawler logic.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from typing import Type
+import csv
+from typing import List, Optional, Type
 
 from selenium.webdriver import Chrome
 from selenium.webdriver.common.by import By
@@ -16,32 +17,56 @@ def get_webelement_text(web_element: WebElement):
     return web_element.text
 
 
-class NaminatedMoviesCrawler:
-    def __init__(self) -> None:
-        self.driver: Chrome = Chrome()
+class NominatedMoviesCrawler:
+    def __init__(self, csv_filename: str = None) -> None:
+        csv_filename = self.get_csv_filename(csv_filename=csv_filename)
+        self.driver: WebDriver = Chrome()
+        field_names = ['year', 'name', 'film_nominations', 'film_awards']
+        self.csvfile = open(f'{csv_filename}', 'w', newline='', encoding='utf-8')
+        self.writer = csv.DictWriter(self.csvfile, fieldnames=field_names)
+        self.writer.writeheader()
         
-    def scrap(self):
+    def scrap(self) -> None:
+        self.nominated_movies: List[dict] = list()
         self.driver.get('https://www.scrapethissite.com/pages/ajax-javascript/')
         year_links: list = reversed(self.driver.find_elements(By.CLASS_NAME, 'year-link')) # type: ignore
         for year_link in year_links:
             year_link.click()
-            movie_table = webdriver_wait(driver=self.driver, timeout=3).until(       # type: ignore
-                EC.visibility_of_element_located((By.ID, 'table-body'))
-            )
-            for movie in movie_table.find_elements(By.CLASS_NAME, 'film'):
-                movie_info_dict = {'year': get_webelement_text(web_element=year_link)}
-                movie_info_dict['name'] = get_webelement_text(web_element=movie.find_element(By.CLASS_NAME, 'film-title'))
-                movie_info_dict['film_nominations'] = get_webelement_text(web_element=movie.find_element(By.CLASS_NAME, 'film-nominations'))
-                movie_info_dict['film_awards'] = get_webelement_text(web_element=movie.find_element(By.CLASS_NAME, 'film-awards'))
-                print(movie_info_dict)
-            
-    # def __exit__(self, exc_type, exc_val, exc_tb):
-    #     self.driver.close()
+            self.scrap_year_movies(year_link=year_link)
+        self.write_to_csv(data=self.nominated_movies)
+                
+    def scrap_year_movies(self, year_link: WebElement) -> None:
+        
+        movie_table: WebElement = webdriver_wait(driver=self.driver, timeout=3).until(       # type: ignore
+            EC.visibility_of_element_located((By.ID, 'table-body'))
+        )
+        for movie in movie_table.find_elements(By.CLASS_NAME, 'film'):
+            movie_info_dict: dict = {'year': get_webelement_text(web_element=year_link)}
+            self.scrap_movie(movie=movie, movie_info_dict=movie_info_dict)
+            self.nominated_movies.append(movie_info_dict)
+                
+    def scrap_movie(self, movie: WebElement, movie_info_dict: dict) -> None:
+        movie_info_dict['name'] = get_webelement_text(web_element=movie.find_element(By.CLASS_NAME, 'film-title'))
+        movie_info_dict['film_nominations'] = get_webelement_text(web_element=movie.find_element(By.CLASS_NAME, 'film-nominations'))
+        movie_info_dict['film_awards'] = get_webelement_text(web_element=movie.find_element(By.CLASS_NAME, 'film-awards'))
+        
+    def write_to_csv(self, data: List[dict]):
+        self.writer.writerows(data)
+    
+    def get_csv_filename(self, csv_filename: Optional[str]) -> str:
+        if csv_filename:
+            if csv_filename.endswith('.csv'):
+                return csv_filename
+            else:
+                return f'{csv_filename}.csv'
+        else:
+            return 'nominated_movies.csv'
     
     def __del__(self):
         self.driver.close()
+        self.csvfile.close()
 
 
 if __name__ == '__main__':
-    crawler: NaminatedMoviesCrawler = NaminatedMoviesCrawler()
+    crawler: NominatedMoviesCrawler = NominatedMoviesCrawler()
     crawler.scrap()
