a11bb2f96fef27c17befaa165a1846da10b715b6
RezuanDzibov <rezuan.dzbov@gmail.com>
2022-01-09 21:09:07 +0300

    Renamed MainCrawler to NaminatedMoviesCrawler and added parsing logic into scrap method.

--- main.py
+++ main.py
@@ -5,13 +5,18 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.remote.webelement import WebElement
 
 
 def webdriver_wait(driver: Type[WebDriver], timeout: int) -> WebDriverWait:
     return WebDriverWait(driver=driver, timeout=timeout)
 
 
-class MainCrawler:
+def get_webelement_text(web_element: WebElement):
+    return web_element.text
+
+
+class NaminatedMoviesCrawler:
     def __init__(self) -> None:
         self.driver: Chrome = Chrome()
         
@@ -23,6 +28,12 @@ class MainCrawler:
             movie_table = webdriver_wait(driver=self.driver, timeout=3).until(       # type: ignore
                 EC.visibility_of_element_located((By.ID, 'table-body'))
             )
+            for movie in movie_table.find_elements(By.CLASS_NAME, 'film'):
+                movie_info_dict = {'year': get_webelement_text(web_element=year_link)}
+                movie_info_dict['name'] = get_webelement_text(web_element=movie.find_element(By.CLASS_NAME, 'film-title'))
+                movie_info_dict['film_nominations'] = get_webelement_text(web_element=movie.find_element(By.CLASS_NAME, 'film-nominations'))
+                movie_info_dict['film_awards'] = get_webelement_text(web_element=movie.find_element(By.CLASS_NAME, 'film-awards'))
+                print(movie_info_dict)
             
     # def __exit__(self, exc_type, exc_val, exc_tb):
     #     self.driver.close()
@@ -32,5 +43,5 @@ class MainCrawler:
 
 
 if __name__ == '__main__':
-    crawler: MainCrawler = MainCrawler()
+    crawler: NaminatedMoviesCrawler = NaminatedMoviesCrawler()
     crawler.scrap()
